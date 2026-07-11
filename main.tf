0ebe40065ab25480b8ae791890900a871bdcdcf7
psnirmala <periasamy.subramanian@gmail.com>
2026-07-11 17:43:03 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "aks" {
+  name     = "aks-production-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "production-aks-cluster"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "prod-aks-cluster"
+
+  default_node_pool {
+    name       = "systempool"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
